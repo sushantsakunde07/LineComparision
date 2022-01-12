6a264b04c91e7134113104e8d3c37efa373bff7c
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-01-12 06:50:54 +0530

    Added: Length of line

--- src/LineComparision/LineComparision.java
+++ src/LineComparision/LineComparision.java
@@ -20,8 +20,8 @@ public class LineComparision {
         y2 = scan.nextInt();
         scan.close();
         length = (Math.sqrt(Math.pow(x2 - x1, 2) + (Math.pow(y2 - y1, 2))));
-        System.out.println("Length of line: " + length);
-        return 0;
+        System.out.println(" Length of line: " + length);
+        return (int) x1;
     }
 
     public static void main(String[] args) {
